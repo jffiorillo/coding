fb1d08312e4bdd6e8ea582afa79ddf0b680848e4
Jose Francisco Fiorillo <jffiorillo@gmail.com>
2018-06-21 10:16:44 +0200

    Initial

---
+++ src/MedianOfTwoSortedArrays.kt
@@ -0,0 +1,10 @@
+fun <T> medianOfTwoSortedArrays(first: List<T>, second: List<T>, kth: Int = (first.size + second.size) / 2) {
+    return when(kth){
+        1 ->
+    }
+}
+
+
+fun main(args: Array<String>) {
+
+}
