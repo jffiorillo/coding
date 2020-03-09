14dc573ac8dc575deaf50eab9174dcf20234d55a
Jose Fiorillo <jose.fiorillo@schibsted.com>
2020-03-09 16:43:20 +0100

    Binary Search Tree

---
+++ src/main/kotlin/io/recursion/BinarySearchTree.kt
@@ -0,0 +1,26 @@
+package io.recursion
+
+import io.tree.TreeNode
+
+//https://leetcode.com/explore/featured/card/recursion-i/251/scenario-i-recurrence-relation/3233/
+class BinarySearchTree {
+
+  fun executeRecursive(root: TreeNode?, target: Int): TreeNode? = when {
+    root == null -> root
+    root.`val` == target -> root
+    root.`val` < target -> executeRecursive(root.right, target)
+    else -> executeRecursive(root.left, target)
+  }
+
+  fun executeIterative(root: TreeNode?, target: Int): TreeNode? {
+    var current = root
+    loop@ while (current != null) {
+      when {
+        current.`val` == target -> break@loop
+        current.`val` < target -> current = current.right
+        else -> current = current.left
+      }
+    }
+    return current
+  }
+}
