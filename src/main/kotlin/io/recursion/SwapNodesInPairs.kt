01c5536b7f4e08b75c8e20e1266b8acbbb01c846
Jose Fiorillo <jose.fiorillo@schibsted.com>
2020-03-09 15:15:50 +0100

    Simplify SwapNodesInPairs

--- src/main/kotlin/io/recursion/SwapNodesInPairs.kt
+++ src/main/kotlin/io/recursion/SwapNodesInPairs.kt
@@ -5,20 +5,11 @@ class SwapNodesInPairs {
   fun execute(node: ListNode?= null): ListNode? =when{
     node == null -> null
     node.next == null -> node
-    node.next?.next == null -> {
-      node.next?.let{ child->
-        child.next = node
-        node.next = null
-        child
-      }
-    }
     else -> {
-      node.next?.next.let{ childChild->
-        node.next?.let { child ->
-          child.next = node
-          node.next = execute(childChild)
-          child
-        }
+      node.next?.let{ next ->
+        node.next = execute(next.next)
+        next.next = node
+        next
       }
     }
   }
