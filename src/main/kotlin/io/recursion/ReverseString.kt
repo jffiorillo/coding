c0a100a730e240aceabf2d76d732e29b54aa2a48
Jose Fiorillo <jose.fiorillo@schibsted.com>
2020-03-09 13:47:01 +0100

    Reverse string

---
+++ src/main/kotlin/io/recursion/ReverseString.kt
@@ -0,0 +1,29 @@
+package io.recursion
+
+//https://leetcode.com/explore/featured/card/recursion-i/250/principle-of-recursion/1440/
+class ReverseString {
+
+  fun execute(input: CharArray, left: Int = 0, right: Int = input.size): Unit = when {
+    left < right -> input[left].let { temp ->
+      input[left] = input[right]
+      input[right] = temp
+      execute(input,left+1,right -1)
+    }
+    else -> {}
+  }
+
+  fun helper(s: CharArray, left: Int, right: Int) {
+    var left = left
+    var right = right
+    if (left >= right) return
+    val tmp = s[left]
+    s[left++] = s[right]
+    s[right--] = tmp
+    helper(s, left, right)
+  }
+}
+
+fun main() {
+  val reverseString = ReverseString()
+  println(reverseString.execute(charArrayOf('h', 'e', 'l', 'l', 'o')))
+}
