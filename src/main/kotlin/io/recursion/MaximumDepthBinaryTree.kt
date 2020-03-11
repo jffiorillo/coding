c5abef56de0bce98efa04e51e0a308bda6d7879f
Jose Fiorillo <jose.fiorillo@schibsted.com>
2020-03-11 13:36:48 +0100

    MaximumDepthBinaryTree

---
+++ src/main/kotlin/io/recursion/MaximumDepthBinaryTree.kt
@@ -0,0 +1,13 @@
+package io.recursion
+
+import io.tree.TreeNode
+import kotlin.math.max
+
+//https://leetcode.com/explore/featured/card/recursion-i/256/complexity-analysis/2375/
+class MaximumDepthBinaryTree {
+
+  fun execute(tree: TreeNode?): Int = when(tree){
+    null -> 0
+    else -> max(execute(tree.left),execute(tree.right)) + 1
+  }
+}
