127cf600ba33e157de53d1895eb2f8ad9205c02c
Jose Fiorillo <jose.fiorillo@schibsted.com>
2020-03-11 14:21:20 +0100

    MergeTwoList

---
+++ src/main/kotlin/io/recursion/MergeTwoList.kt
@@ -0,0 +1,18 @@
+package io.recursion
+
+//https://leetcode.com/explore/featured/card/recursion-i/253/conclusion/2382/
+class MergeTwoList {
+
+  fun execute(list: ListNode?, list1: ListNode?): ListNode? = when {
+    list == null -> list1
+    list1 == null -> list
+    list1.`val` > list.`val` ->
+      ListNode(list.`val`).apply {
+        next = execute(list.next, list1)
+      }
+    else ->
+      ListNode(list1.`val`).apply {
+        next = execute(list, list1.next)
+      }
+  }
+}
