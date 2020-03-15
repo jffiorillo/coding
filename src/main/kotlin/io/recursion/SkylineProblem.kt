6cc796b9ddf669ceaa482dde628c46eb0c6f82b3
Jose Fiorillo <jose.fiorillo@schibsted.com>
2020-03-15 16:06:03 +0100

    SkylineProblem

---
+++ src/main/kotlin/io/recursion/SkylineProblem.kt
@@ -0,0 +1,61 @@
+package io.recursion
+
+// https://leetcode.com/explore/learn/card/recursion-ii/507/beyond-recursion/3006/
+class SkylineProblem {
+
+  fun execute(buildings: Array<IntArray>): List<List<Int>> {
+    val sortDots = convertToPoints(buildings)
+    val currentArea = mutableListOf<Int>()
+    val result: MutableList<List<Int>> = mutableListOf()
+    var currentAreaMax = 0
+    sortDots.forEach { element ->
+      when (element.type) {
+        BuildingPointType.BEGIN -> {
+          currentArea.add(element.height)
+          if (element.height > currentAreaMax) {
+            currentAreaMax = element.height
+            result.add(element.toList())
+          }
+        }
+        BuildingPointType.END -> {
+          currentArea.remove(element.height)
+          val newCurrentAreaMax = currentArea.max() ?: 0
+          if (newCurrentAreaMax != currentAreaMax) {
+            currentAreaMax = newCurrentAreaMax
+            result.add(listOf(element.x, currentAreaMax))
+          }
+        }
+      }
+    }
+    return result
+  }
+
+  fun convertToPoints(buildings: Array<IntArray>): List<BuildingPoint> =
+      buildings.flatMap {
+        listOf(BuildingPoint(it[0], it[2], BuildingPointType.BEGIN),
+            BuildingPoint(it[1], it[2], BuildingPointType.END))
+      }.sorted()
+
+  data class BuildingPoint(val x: Int, val height: Int, val type: BuildingPointType) : Comparable<BuildingPoint> {
+    fun toList() = listOf(x, height)
+    override fun compareTo(other: BuildingPoint): Int =
+        if (this.x != other.x) {
+          this.x - other.x
+        } else {
+          (if (this.type == BuildingPointType.BEGIN) -this.height else this.height) -
+              (if (other.type == BuildingPointType.BEGIN) -other.height else other.height)
+        }
+  }
+
+  enum class BuildingPointType {
+    BEGIN, END
+  }
+
+}
+
+fun main() {
+  val skylineProblem = SkylineProblem()
+  skylineProblem.convertToPoints(arrayOf(intArrayOf(0, 2, 3), intArrayOf(0, 1, 2), intArrayOf(3, 5, 3), intArrayOf(4, 5, 2))).map {
+    println(it)
+  }
+}
