38fd1f1e0f4266090224bc5b489c11a58a3a35c7
Jose Fiorillo <jose.fiorillo@schibsted.com>
2020-03-28 18:44:42 +0100

    Optimaze RemoveNthNodeFromEndOfLinkedList

--- src/main/kotlin/io/linkedlist/RemoveNthNodeFromEndOfLinkedList.kt
+++ src/main/kotlin/io/linkedlist/RemoveNthNodeFromEndOfLinkedList.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package io.linkedlist
 
 import io.models.ListNode
@@ -5,18 +7,17 @@ import io.models.ListNode
 // https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1296/
 class RemoveNthNodeFromEndOfLinkedList {
 
-  fun execute(head: ListNode?, n: Int): ListNode? = when (head?.size()) {
-    null, 1 -> null
-    n -> head.next
+  fun execute(head: ListNode?, n: Int): ListNode? = when (head) {
+    null -> null
     else -> {
-      val size = head.size()
-      if (n < size) {
-        var current: ListNode = head
-        repeat(size - n-1) {
-          current = current.next!!
-        }
-        current.next = current.next!!.next
+      var faster: ListNode = head
+      var slower: ListNode = head
+      repeat(n) { faster = faster.next ?: return head.next }
+      while (faster.next != null) {
+        faster = faster.next!!
+        slower = slower.next!!
       }
+      slower.next = slower.next!!.next
       head
     }
   }
