192d38d0064bc5138d708d3e471ba91efce1aa15
Jose Fiorillo <jose.fiorillo@schibsted.com>
2020-03-28 17:39:52 +0100

    Return beginning of the cycle node in linked list

--- src/main/kotlin/io/linkedlist/LinkedListHasCycle.kt
+++ src/main/kotlin/io/linkedlist/LinkedListHasCycle.kt
@@ -1,20 +1,40 @@
 package io.linkedlist
 
 // https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1212/
+// https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1214/
 class LinkedListHasCycle {
 
+  data class ListNode(var `val`: Int, var next: ListNode? = null)
+
   fun execute(head: ListNode?): Boolean {
-    var fast: ListNode? = head?.next
-    var slow: ListNode? = head
-    while (fast != null){
-      fast = fast.next?.next
-      slow = slow?.next
-      if (fast == slow)
+    var faster: ListNode? = head?.next
+    var slower: ListNode? = head
+    while (faster != null){
+      faster = faster.next?.next
+      slower = slower?.next
+      if (faster == slower)
         return true
     }
     return false
   }
 
-  data class ListNode(var `val`: Int, var next: ListNode? = null)
+
+  fun getListNodeCycle(head:ListNode?): ListNode? {
+    var faster: ListNode? = head
+    var slower: ListNode? = head
+    while (faster != null){
+      faster = faster.next?.next
+      slower = slower?.next
+      if (faster != null && faster == slower){
+        slower = head
+        while(faster != slower){
+          slower = slower!!.next
+          faster = faster!!.next
+        }
+        return slower
+      }
+    }
+    return null
+  }
 }
 
