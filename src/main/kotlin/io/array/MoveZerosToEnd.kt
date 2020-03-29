e57b2ad00cbbea3e33c2398c3dc8a08be9743151
Jose Fiorillo <jose.fiorillo@schibsted.com>
2020-03-29 15:02:47 +0200

    Improve MoveZerosToEnd

--- src/main/kotlin/io/array/MoveZerosToEnd.kt
+++ src/main/kotlin/io/array/MoveZerosToEnd.kt
@@ -2,41 +2,30 @@ package io.array
 
 class MoveZerosToEnd {
   fun execute(input: IntArray) = input.apply {
-    var index = 0
-    var firstZeroIndex = -1
-    while (index < size) {
-      if (this[index] == 0) {
-        if (firstZeroIndex == -1) firstZeroIndex = index
-        val nextNotZeroElement = nextNotZeroElement(input, index + 1)
-        swapValues(input, firstZeroIndex, nextNotZeroElement)
-        index = nextNotZeroElement
+    var count = 0
 
-      } else if (firstZeroIndex != -1) {
-        swapValues(input, firstZeroIndex, index)
+    input.forEach { value ->
+      if (value != 0) {
+        input[count] = value
+        count++
       }
-      if (firstZeroIndex != -1) firstZeroIndex++
-      index++
     }
-  }
-
-  private fun swapValues(input: IntArray, firstZeroIndex: Int, nextNotZeroElement: Int) {
-    if (nextNotZeroElement != input.size) {
-      input[firstZeroIndex] = input[nextNotZeroElement]
-      input[nextNotZeroElement] = 0
-    }
-  }
-
-  private fun nextNotZeroElement(input: IntArray, index: Int): Int {
-    var nextNotZeroElement = index
-    while (nextNotZeroElement < input.size && input[nextNotZeroElement] == 0) {
-      nextNotZeroElement++
-    }
-    return nextNotZeroElement
+    (count until input.size).forEach { input[it] = 0 }
   }
 }
 
 fun main() {
   val moveZerosToEnd = MoveZerosToEnd()
-  println(moveZerosToEnd.execute(intArrayOf(1, 2, 0, 4, 3, 0, 0, 0, 5, 0)).toList())
+  listOf(
+      intArrayOf(1, 2, 0, 4, 3, 0, 0, 0, 5, 0) to listOf(1, 2, 4, 3, 5, 0, 0, 0, 0, 0)
+
+  ).map { (input, value) ->
+    val output = moveZerosToEnd.execute(input).toList()
+    val isValid = output == value
+    if (isValid)
+      println("$output is valid")
+    else
+      println("Except $value but instead got $output")
+  }
 
 }
