a97f33d9e01edc3e16727545393e69f0516a1db1
Jose Fiorillo <jose.fiorillo@schibsted.com>
2020-05-03 12:24:26 +0200

    ValidPalindrome

---
+++ src/main/kotlin/io/string/ValidPalindrome.kt
@@ -0,0 +1,28 @@
+package io.string
+
+import io.utils.runTests
+
+// https://leetcode.com/problems/valid-palindrome/
+class ValidPalindrome {
+
+  fun execute(input: String): Boolean {
+    val cleanInput = input.toLowerCase().replace("[^a-z0-9]".toRegex(), "")
+    var start = 0
+    var end = cleanInput.lastIndex
+    while (start < end) {
+      if (cleanInput[start] != cleanInput[end]) {
+        return false
+      }
+      start++
+      end--
+    }
+    return true
+  }
+}
+
+fun main() {
+  runTests(listOf(
+      "A man, a plan, a canal: Panama" to true
+//      "race a car" to false
+  )) { (input, value) -> value to ValidPalindrome().execute(input) }
+}
