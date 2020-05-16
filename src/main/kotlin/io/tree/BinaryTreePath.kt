64e9327f10c7381f31991d86fd6ace45759a7fae
Jose Fiorillo <jose.fiorillo@schibsted.com>
2020-05-16 21:30:00 +0200

    BinaryTreePath

--- src/main/kotlin/io/tree/BinaryTreePath.kt
+++ src/main/kotlin/io/tree/BinaryTreePath.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package io.tree
 
 import io.models.TreeNode
@@ -11,10 +13,8 @@ class BinaryTreePath {
     root == null -> accum
     root.left == null && root.right == null -> accum.apply { add(path + "${root.`val`}") }
     else -> accum.apply {
-      if (root.left != null)
-        execute(root.left, "$path${root.`val`}->", accum)
-      if (root.right != null)
-        execute(root.right, "$path${root.`val`}->", accum)
+      if (root.left != null) execute(root.left, "$path${root.`val`}->", accum)
+      if (root.right != null) execute(root.right, "$path${root.`val`}->", accum)
     }
   }
 }
